c7068e490757c2c790234baf3cd30f515621692e
seows <seows@laivdata.com>
2022-06-27 17:30:24 +0900

    fetchGraphQL의 headers에 Parse app id 전달

--- src/relay/fetchGraphQL.ts
+++ src/relay/fetchGraphQL.ts
@@ -1,15 +1,16 @@
-import { RequestParameters } from 'relay-runtime/lib/util/RelayConcreteNode';
-import { Variables } from 'relay-runtime/lib/util/RelayRuntimeTypes';
+import { RequestParameters } from "relay-runtime/lib/util/RelayConcreteNode";
+import { Variables } from "relay-runtime/lib/util/RelayRuntimeTypes";
 
 export const fetchGraphQL = async (
   request: RequestParameters,
   variables: Variables
 ) => {
-  const response = await fetch('http://localhost:8000/graphql', {
-    method: 'POST',
+  const response = await fetch(process.env.REACT_APP_SERVER_URL as string, {
+    method: "POST",
     headers: {
-      Accept: 'application/json',
-      'Content-type': 'application/json',
+      Accept: "application/json",
+      "Content-type": "application/json",
+      "X-Parse-Application-Id": process.env.REACT_APP_PARSE_APP_ID as string,
     },
     body: JSON.stringify({
       query: request.text,
