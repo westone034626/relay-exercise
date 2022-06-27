a2214ab32c90323acf1b2a349b32605185adbdc8
seows <seows@laivdata.com>
2022-06-27 17:33:00 +0900

    백엔드 api로부터 schema.graphql 받아오는 scripts문 등록, schema.graphql 다운로드

---
+++ src/__generated__/AppQuery.graphql.ts
@@ -0,0 +1,108 @@
+/**
+ * @generated SignedSource<<502ed576d7b7687400825e7aba02910e>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+export type AppQuery$variables = {};
+export type AppQuery$data = {
+  readonly todos: {
+    readonly edges: ReadonlyArray<{
+      readonly node: {
+        readonly id: string;
+        readonly title: string | null;
+      } | null;
+    } | null> | null;
+  };
+};
+export type AppQuery = {
+  response: AppQuery$data;
+  variables: AppQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "alias": null,
+    "args": null,
+    "concreteType": "TodoConnection",
+    "kind": "LinkedField",
+    "name": "todos",
+    "plural": false,
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "TodoEdge",
+        "kind": "LinkedField",
+        "name": "edges",
+        "plural": true,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Todo",
+            "kind": "LinkedField",
+            "name": "node",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "id",
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "title",
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "AppQuery",
+    "selections": (v0/*: any*/),
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "AppQuery",
+    "selections": (v0/*: any*/)
+  },
+  "params": {
+    "cacheID": "69867f1667e58f6e9dd5204ea98d9cec",
+    "id": null,
+    "metadata": {},
+    "name": "AppQuery",
+    "operationKind": "query",
+    "text": "query AppQuery {\n  todos {\n    edges {\n      node {\n        id\n        title\n      }\n    }\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "9c1c19e0dcb8aea8607f7aa4c313a4b2";
+
+export default node;
