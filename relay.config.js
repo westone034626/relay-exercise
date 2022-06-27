3ba7e28469965d9211446ed98b1e35eee653cc78
seows <seows@laivdata.com>
2022-06-27 17:32:13 +0900

    relay.config.js 작성(relay 공식문서 참조)

--- relay.config.js
+++ relay.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  src: '.',
-  schema: './schema.graphql',
-  language: 'typescript',
-  artifactDirectory: './__generated__',
+  src: "./src/",
+  schema: "./schema.graphql",
+  language: "typescript",
+  artifactDirectory: "./__generated__",
+  exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
 };
