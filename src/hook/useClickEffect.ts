d2d5adc394d1dfbcd9efca0edba3132661a41e0a
seows <seows@laivdata.com>
2022-07-05 13:05:04 +0900

    useClickEffect hook 추가

---
+++ src/hook/useClickEffect.ts
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from "react";
+
+function useClickEffect<T extends HTMLElement>(
+  ref: React.MutableRefObject<T | null>,
+  effect: (data: boolean) => void
+) {
+  useEffect(() => {
+    function touchHandler(e: TouchEvent) {
+      e.target instanceof Element &&
+        effect(Boolean(ref.current?.contains(e.target)));
+    }
+    document.addEventListener("touchstart", touchHandler);
+    return () => document.removeEventListener("touchstart", touchHandler);
+  }, []);
+}
+
+export default useClickEffect;
