96bbc4fb14b3397bb828beeb376d154407bbc95c
westone034626 <dydrlaks@likelion.org>
2022-09-03 16:52:45 +0900

    LoginScreen 컴포넌트 내부에 간격 조정 & LoginForm 컴포넌트 호출

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import LoginForm from "../components/LoginScreen/LoginForm";
 import SectionTitle from "../components/SectionTitle";
+import WhiteSpace from "../components/WhiteSpace";
 
 function LoginScreen() {
   return (
     <section>
-      <SectionTitle title="로그인" />
+      <WhiteSpace size={"md"} />
+      <SectionTitle title="로그인" style={{ marginLeft: 16 }} />
+      <WhiteSpace size={"xl"} />
+      <LoginForm />
     </section>
   );
 }
