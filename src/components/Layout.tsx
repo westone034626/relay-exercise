73764fe08863ce5d979b8f6df290972ac53d1203
westone034626 <dydrlaks@likelion.org>
2022-09-03 15:37:20 +0900

    NavigationBar 컴포넌트에 로그인, 로그아웃 버튼 삽입

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavigationBar from "./NavigationBar";
+import LoginButton from "./NavigationBar/LoginButton";
+import SignUpButton from "./NavigationBar/SignUpButton";
 
 function Layout() {
   return (
     <>
-      <NavigationBar />
+      <NavigationBar
+        style={{ padding: 24 }}
+        right={
+          <div style={{ display: "flex", gap: "0 24px" }}>
+            <LoginButton />
+            <SignUpButton />
+          </div>
+        }
+      />
       <Outlet />
     </>
   );
