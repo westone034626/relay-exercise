88f18f48cb24e08d3796e197fa78cfdee03a8b60
seows <seows@laivdata.com>
2022-09-08 23:37:52 +0900

    SignUpForm, SignUpScreen UI 구현

---
+++ src/components/SignUpScreen/SignUpForm.tsx
@@ -0,0 +1,100 @@
+import styles from "./SignUpForm.module.css";
+import { CSSProperties } from "react";
+import { Form, Input } from "antd";
+import { ValidateErrorEntity } from "rc-field-form/lib/interface";
+
+export interface SignUpFormData {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}
+interface SignUpForm {
+  style?: CSSProperties;
+  onFinish: (data: SignUpFormData) => void;
+  onFinishFailed: (errorInfo: ValidateErrorEntity<SignUpFormData>) => void;
+}
+
+export default function SignUpForm({
+  style,
+  onFinish,
+  onFinishFailed,
+}: SignUpForm) {
+  return (
+    <Form
+      style={style}
+      name="signUpForm"
+      wrapperCol={{ span: 24 }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        name="email"
+        rules={[
+          { required: true, message: "이메일을 입력해주세요." },
+          { type: "email", message: "이메일 형식으로 입력해주세요." },
+        ]}
+        hasFeedback
+      >
+        <Input placeholder="이메일" style={getInputItemStyle()} />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: "비밀번호를 입력해주세요." }]}
+        hasFeedback
+      >
+        <Input
+          type="password"
+          placeholder="비밀번호"
+          style={getInputItemStyle()}
+        />
+      </Form.Item>
+
+      <Form.Item
+        name="passwordConfirm"
+        dependencies={["password"]}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "비밀번호 확인을 진행해주세요.",
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue("password") === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error("비밀번호가 일치하지 않습니다."));
+            },
+          }),
+        ]}
+      >
+        <Input
+          type="password"
+          placeholder="비밀번호 확인"
+          style={getInputItemStyle()}
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <button
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          className={styles.submitButton}
+        >
+          회원가입
+        </button>
+      </Form.Item>
+    </Form>
+  );
+
+  function getInputItemStyle(): CSSProperties {
+    return {
+      padding: 12,
+      backgroundColor: "#ffffff",
+      borderRadius: 12,
+      width: "100%",
+    };
+  }
+}
--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import SectionTitle from "../components/SectionTitle";
+import SignUpForm, {
+  SignUpFormData,
+} from "../components/SignUpScreen/SignUpForm";
+import WhiteSpace from "../components/WhiteSpace";
+import styles from "./LoginScreen.module.css";
 
 function SignUpScreen() {
-  return <div>SignUpScreen</div>;
+  const onSignUpFormFinish = (data: SignUpFormData) => {
+    console.log(data);
+  };
+  const onSignUpFormFinishFailed = () => {};
+  return (
+    <section className={styles.container}>
+      <WhiteSpace size={"md"} />
+      <SectionTitle title="회원가입" style={{ marginLeft: 16 }} />
+      <WhiteSpace size={"xl"} />
+      <SignUpForm
+        style={{ padding: "0 36px" }}
+        onFinish={onSignUpFormFinish}
+        onFinishFailed={onSignUpFormFinishFailed}
+      />
+    </section>
+  );
 }
 
 export default SignUpScreen;
