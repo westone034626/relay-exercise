18ccf25e76db5915ef62cc113db0412a727d684f
westone034626 <dydrlaks@likelion.org>
2022-09-09 21:30:41 +0900

    최상단 Suspense에서 fallback으로 NavigationBar 보여주도록 조정

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,12 +4,21 @@ import Providers from "./Providers";
 import App from "./App";
 import Loading from "./Loading";
 import ErrorBoundaryRetry from "./ErrorBoundaryRetry";
+import NavigationBar from "./components/NavigationBar";
 
 const Root = () => {
   return (
     <Providers>
       <ErrorBoundaryRetry>
-        <Suspense fallback={<Loading />}>
+        {/* <Suspense fallback={<Loading />}> */}
+        <Suspense
+          fallback={
+            <>
+              <NavigationBar style={{ padding: 24 }} />
+              <Loading />
+            </>
+          }
+        >
           <App />
         </Suspense>
       </ErrorBoundaryRetry>
