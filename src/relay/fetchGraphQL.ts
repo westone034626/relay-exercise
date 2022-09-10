9b617fd93f9be9bf4152f0ed0b585b986459512c
westone034626 <dydrlaks@likelion.org>
2022-09-10 22:23:18 +0900

    fetchGraphQL에서 header에 sessionId를 싣도록 조치

--- src/relay/fetchGraphQL.ts
+++ src/relay/fetchGraphQL.ts
@@ -5,12 +5,14 @@ export const fetchGraphQL = async (
   request: RequestParameters,
   variables: Variables
 ) => {
+  const sessionId = sessionStorage.getItem("todayChecksSessionId");
   const response = await fetch(process.env.REACT_APP_SERVER_URL as string, {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-type": "application/json",
       "X-Parse-Application-Id": process.env.REACT_APP_PARSE_APP_ID as string,
+      ...(sessionId && { "X-Parse-Session-Token": JSON.parse(sessionId) }),
     },
     body: JSON.stringify({
       query: request.text,
