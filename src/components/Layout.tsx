3e209cf30e6f54bb72af2285c958d67ff10a6f43
westone034626 <dydrlaks@likelion.org>
2022-09-11 00:29:26 +0900

    로그인 상태일 때 네비바에 로그아웃이 노출되도록 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,36 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useAuth } from "./AuthProvider";
 import NavigationBar from "./NavigationBar";
 import LoginButton from "./NavigationBar/LoginButton";
+import LogoutButton from "./NavigationBar/LogoutButton";
 import SignUpButton from "./NavigationBar/SignUpButton";
 
 function Layout() {
+  const auth = useAuth();
+  const navigation = useNavigate();
+  const goHome = () => {
+    navigation("/");
+  };
   return (
     <>
       <NavigationBar
         style={{ padding: 24 }}
         right={
           <div style={{ display: "flex", gap: "0 24px" }}>
-            <LoginButton />
-            <SignUpButton />
+            {auth?.user ? (
+              <LogoutButton
+                onClick={() => {
+                  auth?.logout().then(() => {
+                    goHome();
+                  });
+                }}
+              />
+            ) : (
+              <>
+                <LoginButton />
+                <SignUpButton />
+              </>
+            )}
           </div>
         }
       />
