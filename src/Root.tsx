d0bf2b9bc0fa5426f4da6dc27be00f78e047499d
westone034626 <dydrlaks@likelion.org>
2022-09-10 22:22:43 +0900

    최상위 Suspense의 fallback을 Loading으로 복귀

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,21 +5,17 @@ import App from "./App";
 import Loading from "./Loading";
 import ErrorBoundaryRetry from "./ErrorBoundaryRetry";
 import NavigationBar from "./components/NavigationBar";
+import { AuthProvider } from "./components/AuthProvider";
 
 const Root = () => {
   return (
     <Providers>
       <ErrorBoundaryRetry>
         {/* <Suspense fallback={<Loading />}> */}
-        <Suspense
-          fallback={
-            <>
-              <NavigationBar style={{ padding: 24 }} />
-              <Loading />
-            </>
-          }
-        >
-          <App />
+        <Suspense fallback={<Loading />}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
         </Suspense>
       </ErrorBoundaryRetry>
     </Providers>
