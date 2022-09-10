945ee6a88766d8a2dca900a5bb15a248209b85e5
westone034626 <dydrlaks@likelion.org>
2022-09-11 00:09:26 +0900

    사용안하는 코드, 주석 제거

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,14 +4,12 @@ import Providers from "./Providers";
 import App from "./App";
 import Loading from "./Loading";
 import ErrorBoundaryRetry from "./ErrorBoundaryRetry";
-import NavigationBar from "./components/NavigationBar";
 import { AuthProvider } from "./components/AuthProvider";
 
 const Root = () => {
   return (
     <Providers>
       <ErrorBoundaryRetry>
-        {/* <Suspense fallback={<Loading />}> */}
         <Suspense fallback={<Loading />}>
           <AuthProvider>
             <App />
